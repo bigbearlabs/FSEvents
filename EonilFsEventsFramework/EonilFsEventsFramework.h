32fd0de55058e5912de80eb5cebc5d2815cd1043
Andy Park <andy@bigbearlabs.com>
2019-04-13 03:03:31 +0200

    add a framework target for carthage.

---
+++ EonilFsEventsFramework/EonilFsEventsFramework.h
@@ -0,0 +1,19 @@
+//
+//  EonilFsEventsFramework.h
+//  EonilFsEventsFramework
+//
+//  Created by ilo on 12/04/2019.
+//  Copyright © 2019 Eonil. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EonilFsEventsFramework.
+FOUNDATION_EXPORT double EonilFsEventsFrameworkVersionNumber;
+
+//! Project version string for EonilFsEventsFramework.
+FOUNDATION_EXPORT const unsigned char EonilFsEventsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EonilFsEventsFramework/PublicHeader.h>
+
+
